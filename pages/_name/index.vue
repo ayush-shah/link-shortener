433808288384c3437890b79d029d00a12f2defa6
Ayush Shah <ayush.shah@deuexsolutions.com>
2020-05-09 22:29:16 +0530

    Link match issue--lowercase/uppercase-bug

--- pages/_name/index.vue
+++ pages/_name/index.vue
@@ -15,17 +15,19 @@ export default {
         Accept: 'application/json',
         'Content-Type': 'application/json'
       },
-      body: JSON.stringify({ name: this.$route.params.name })
+      body: JSON.stringify({ name: this.$route.params.name.toUpperCase() })
+    }).then(response => {
+      if (response.status === 200) {
+        response.json().then(data => {
+          if (data['link'] !== undefined) {
+            this.link = 'Going to ' + data['link']
+            location.replace('https://' + data['link'])
+          } else {
+            this.link = 'No allocation of any link to this name'
+          }
+        })
+      }
     })
-      .then(response => response.json())
-      .then(data => {
-        if (data['link'] !== undefined) {
-          this.link = 'Going to ' + data['link']
-          location.replace('https://' + data['link'])
-        } else {
-          this.link = 'No allocation of any link to this name'
-        }
-      })
   }
 }
 </script>
